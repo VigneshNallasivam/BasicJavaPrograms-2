5c7333511dcf13c8f1a6c9a813063e4317366e37
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-24 20:43:46 +0530

    Added : Reversing the Given Number

---
+++ src/com/Java/basics/ReversingNumber.java
@@ -0,0 +1,24 @@
+package com.Java.basics;
+import java.util.Scanner;
+public class ReversingNumber 
+{
+	   public static void main(String[] args)
+		{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		int number = scan.nextInt();
+		
+		int reverse=0;
+		int remainder;
+		int temp = number;
+		
+		while(number>0)
+		{
+			remainder = number%10;           
+			reverse = (reverse*10) + remainder;  
+			number = number/10;              
+		}
+		
+		System.out.println("The Reverse of a Number Is : " +reverse);
+		}
+}
