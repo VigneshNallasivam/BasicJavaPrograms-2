d00b5767749a715272a47a6c17755d042fd90b9f
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-24 20:49:25 +0530

    Added : Performing the Addition of given Natural Numbers using While Loop

---
+++ src/com/Java/basics/NaturalNumbersSum.java
@@ -0,0 +1,15 @@
+package com.Java.basics;
+
+public class NaturalNumbersSum 
+{
+	public static void main(String[] args)   
+	{  
+	int Numbers = 50, i=1, sum = 0;    
+	while(i<=Numbers)  
+	{   
+	sum=sum + i;    
+	i++;  
+	}     
+	System.out.println("Sum of Natural Numbers is = " + sum);  
+	}  
+}
