f396665bd18459f290b3356625cd42742be98306
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-24 19:44:59 +0530

    Added : Printing the number of a Month where month name is the Input

---
+++ src/com/Java/basics/MonthNumber.java
@@ -0,0 +1,48 @@
+package com.Java.basics;
+
+import java.util.Scanner;
+public class MonthNumber 
+{
+	public static void main(String s[])
+	 {
+	 Scanner scanner = new Scanner(System.in);
+	 System.out.print("Enter Name of the Month : ");
+	 String name=scanner.next();
+     switch(name)
+	 {
+	           case"JANUARY":System.out.println("Month Number: 1");
+	                         break;
+	           case"FEBRUARY":System.out.println("Month Number: 2");
+	                          break;
+	           case"MARCH":System.out.println("Month Number: 3");
+	                       break;
+	           case"APRIL":System.out.println("Month Number: 4");
+	                       break;
+	           case"MAY":System.out.println("Month Number: 5");
+	                     break;
+	           case "JUNE":System.out.println("Month Number: 6");
+	                       break;
+		       case"JULY":System.out.println("Month Number: 7");
+	                          break;
+	           case"AUGUST":System.out.println("Month Number: 8");
+	                            break;
+
+		        case"SEPTEMBER":System.out.println("Month Number: 9");
+	                               break;
+
+		        case"OCTOBER":System.out.println("Month Number: 10");
+	                             break;
+
+		        case"NOVEMBER":System.out.println("Month Number: 11");
+	                              break;
+
+		         case"DECEMBER":System.out.println("Month Number: 12");
+		                          break;
+		           
+	             default: System.out.println("Invalid Data.");
+
+		}
+
+	 }
+
+}
