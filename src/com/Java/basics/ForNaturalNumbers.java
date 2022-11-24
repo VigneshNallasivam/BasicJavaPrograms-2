399245cfa6fc2fca89879bbb833d736bb058f3d8
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-24 20:56:28 +0530

    Added : Performing the Addition of given Natural Numbers using For loop

---
+++ src/com/Java/basics/ForNaturalNumbers.java
@@ -0,0 +1,14 @@
+package com.Java.basics;
+
+public class ForNaturalNumbers 
+{
+	public static void main(String[] args)   
+	{  
+	int Number = 75, sum = 0, i;    
+	for(i = 1; i <= Number ; i++)  
+	{  
+	sum = sum + i;  
+	}  
+	System.out.println("Sum of Natural Numbers is = " + sum);  
+	}  
+}
