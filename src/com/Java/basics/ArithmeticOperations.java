75a990974a84f02e3b29fe6409342292de2df556
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-24 19:37:07 +0530

    Added : Performing addition, subtraction, Multiplication & Division in class Main Function

---
+++ src/com/Java/basics/ArithmeticOperations.java
@@ -0,0 +1,25 @@
+package com.Java.basics;
+
+public class ArithmeticOperations 
+{
+	public static void main(String[] args)
+	{
+		int a=10,b=20,c=30;
+		int W;
+		float X;
+		long Y;
+		int Z;
+		
+		W=a+b*c;
+		System.out.println("1) W = a+b*c =  "+W);
+		
+		X=c+a/b;
+		System.out.println("2) X = c+a/b =  "+X);
+		
+		Y=a%b+c;
+		System.out.println("3) Y = a%b+c =  "+Y);
+	    
+		Z=a*b+c;
+	    System.out.println("4) Z = a*b+c =  "+Z);
+	 }
+}
